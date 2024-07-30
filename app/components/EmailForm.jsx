a32a90d291062e0e76e3f1cde0a227389ff88e71
Zakari Adamu <ojonimizakari@gmail.com>
2024-07-30 13:37:31 +0100

    fix typo apostrophy error warning with &apos;

--- app/components/EmailForm.jsx
+++ app/components/EmailForm.jsx
@@ -8,7 +8,9 @@ const EmailForm = () => {
 	return (
 		<section className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4">
 			<div>
-				<h5 className="text-xl font-bold text-white my-2">Let's connect</h5>
+				<h5 className="text-xl font-bold text-white my-2">
+					Let&apos;s connect
+				</h5>
 				<p className="text-[#ADB7BE] mb-4 max-w-md">
 					I&apos;m currently looking for new opportunities to contribute my
 					skills, learn new skills and grow. Whether you have a question or just
