b6a6f21f782254dc5fd5bada9a0ac73bb7a2f861
Zakari Adamu <53929284+ZakariAdamu@users.noreply.github.com>
2024-08-06 17:55:33 +0100

    Update About.jsx

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -61,12 +61,12 @@ const About = () => {
 				<div className="mt-4 md:mt-0 text-left flex flex-col h-full">
 					<h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
 					<p className="text-base lg:text-lg">
-						I am a Front-end web developer with a passion for creating
+						Full stack web developer with a passion for creating
 						interactive and responsive web applications. I have experience
-						working with JavaScript, TypeScript, React, Node.js, Express,
-						MongoDB, HTML, CSS, and Git. I am a quick learner and I am always
+						working with Next.js, TypeScript, React, Node.js, Express,
+						MongoDB, HTML, CSS, and Git/GitHub. Am a quick learner and am always
 						looking to expand my knowledge and skill set. I am a team player and
-						I am excited to work with others to create amazing applications.
+						ever excited to work with others to create amazing applications.
 					</p>
 					<div className="flex flex-row mt-8">
 						<TabButton
