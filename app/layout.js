43412b39c828ae5e2f0bbe0ba0a0c83f5601a564
Zakari Adamu <ojonimizakari@gmail.com>
2024-08-15 17:31:25 +0100

    changed my favicon

--- app/layout.js
+++ app/layout.js
@@ -4,14 +4,14 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Zakari's Portfolio",
-  description: "Personal porfolio website showcasing my skills and web projects.",
+	title: "Zakari's Portfolio",
+	description:
+		"Personal porfolio website showcasing my skills and web projects.",
 };
 
 export default function RootLayout({ children }) {
-  return (
+	return (
 		<html lang="en">
-			<link rel="icon" href="/favicon.ico" />
 			<body className={inter.className}>{children}</body>
 		</html>
 	);
